52c0315939d5cac9b43d23dfbedae243df4fe6ff
Eric Juta <ericjohnjuta@gmail.com>
2021-01-24 20:41:39 +0000

    trim and bump solidity version

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -29,7 +29,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.7.3",
+        version: "0.8.0",
         settings: {
           optimizer: {
             enabled: true,
--- test/sample-test.ts
+++ test/sample-test.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { Signer } from "ethers";
 import { assert } from "chai";
 
-describe("Token", function () {
+describe("Greeter", function () {
   let accounts: Signer[];
 
   beforeEach(async function () {
@@ -10,6 +10,7 @@ describe("Token", function () {
   });
 
   it("should have accounts", async function () {
+    // console.log(accounts);
     assert(accounts.length > 0, "Account legth should be more then zero");
   });
 });
