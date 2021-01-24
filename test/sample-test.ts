0719669c0a6fc980eb7971aca86ff52cc6df1e93
Eric Juta <ericjohnjuta@gmail.com>
2021-01-24 21:37:04 +0000

    Update sample test

--- test/sample-test.ts
+++ test/sample-test.ts
@@ -1,6 +1,6 @@
 import { ethers } from "hardhat";
 import { Signer } from "ethers";
-import { assert } from "chai";
+import { assert, expect } from "chai";
 
 describe("Greeter", function () {
   let accounts: Signer[];
@@ -12,5 +12,13 @@ describe("Greeter", function () {
   it("should have accounts", async function () {
     // console.log(accounts);
     assert(accounts.length > 0, "Account legth should be more then zero");
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+
+    await greeter.deployed();
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    await greeter.setGreeting("Hola, mundo!");
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
 });
